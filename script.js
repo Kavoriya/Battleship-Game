f357193781c020345694ae4dd09e3c4ee3f1e577
Roman <kavoriya@gmail.com>
2023-08-14 17:29:02 +0500

    Build: Gameboard class, Cell class and addShip method

--- script.js
+++ script.js
@@ -18,4 +18,35 @@ class Ship {
 
 }
 
-export { Ship };
+class Cell {
+   constructor() {
+      this.isHit = false;
+      this.ship = null;
+   }
+}
+
+class Gameboard {
+   constructor() {
+      this.rows = 10;
+      this.columns = 10;
+      this.shipsList = [];
+      this.gameboard = [];
+      for (let i = 0; i < this.rows; i++) {
+         let row = [];
+         for (let j = 0; j < this.columns; j++) {
+            row.push(new Cell);
+         }
+         this.gameboard.push(row);
+      }
+   }
+
+   addShip(coords) {
+      let newShip = new Ship(coords.length);
+      coords.forEach(coord => {
+         this.gameboard[coord[0]][coord[1]].ship = newShip;
+      })
+   }
+
+}
+
+export { Ship, Cell, Gameboard };
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-import { Ship } from './script.js';
+import { Ship, Cell, Gameboard } from './script.js';
 
 test('A ship gets sunk when getting hit == "length" times', () => {
    let ship = new Ship(2);
@@ -14,4 +14,17 @@ test('A ship is not sunk when getting hit < "length" times', () => {
       ship.hit();
    }
   expect(ship.isSunk).toBe(false);
+});
+
+test('Gameboard adds a 1 cell ship correctly', () => {
+   let gameboard = new Gameboard;
+   gameboard.addShip([[0, 5]]);
+  expect(gameboard.gameboard[0][5].ship).not.toBeNull();
+});
+
+test('Gameboard adds a 2 cell ship correctly', () => {
+   let gameboard = new Gameboard;
+   gameboard.addShip([[0, 5], [0, 6]]);
+  expect(gameboard.gameboard[0][5].ship).not.toBeNull();
+  expect(gameboard.gameboard[0][6].ship).not.toBeNull();
 });
